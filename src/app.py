d2ff05d348553492916f3487ee3f17a24b945d5c
Oswaldo Peralta <oswaldoperalta@gmail.com>
2020-06-02 18:53:52 -0400

    new server ip

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy # ORM for database layer abstraction
 from flask_marshmallow import Marshmallow # allow db schema creation
 
 app = Flask(__name__) #set aplication name
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://levely:Elex.184@35.215.208.159/flaskmysql' # pass db parameters
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://levely:Elex.184@35.215.208.159/flaskmysql' # pass db parameters new server
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #skip warning messages on runtime
 
 db = SQLAlchemy(app) # initialize variable
