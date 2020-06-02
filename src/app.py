9bc5b4cedfa4a81de727fe542c17f773aa162783
Oswaldo Peralta <oswaldoperalta@gmail.com>
2020-06-02 10:02:39 -0400

    first run

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
-from flask import Flask # framework to build webapps
-from flask-sqlalchemy import SQLAlchemy # ORM for database layer abstraction
+from flask import Flask, request # framework to build webapps
+from flask_sqlalchemy import SQLAlchemy # ORM for database layer abstraction
 from flask_marshmallow import Marshmallow # allow db schema creation
 
-app = flask(__name__) #set aplication name
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://levely:Elex.184@35.215.208.159/flaskmysql' # pass db parameters
+app = Flask(__name__) #set aplication name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://levely:Elex.184@35.215.208.159/flaskmysql' # pass db parameters
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #skip warning messages on runtime
 
-db = SQLALCHEMY(app) # pass config to database variable
-ma = Marshmallow(app) # pass config to schema variable
+db = SQLAlchemy(app) # initialize variable
+ma = Marshmallow(app) # initialize variable
 
 ## DATABASE MODELLING ##
 
@@ -19,7 +19,7 @@ class student(db.Model): # define table with columns below
     age = db.Column(db.Integer)
     course = db.Column(db.String(70))
 
-    def __init__(self, rut, name, last_name, age, course) # assign incoming data to columns
+    def __init__(self, rut, name, last_name, age, course): # assign incoming data to columns
         self.rut = rut
         self.name = name
         self.last_name = last_name
@@ -28,8 +28,21 @@ class student(db.Model): # define table with columns below
 
 db.create_all() # method to read all classes and create tables
 
-class studentSchema(ma.Schema): # set db schema fields
+class studentSchema(ma.Schema): # set db schema
     class Meta:
         fields = ('id', 'rut', 'name', 'last_name', 'age', 'course')
 
 student_schema = studentSchema # create variable instance to be used elsewhere
+
+## ENDPOINTS ##
+
+@app.route('/student', methods=['POST'])
+def createStudent():
+
+    print(request.json)
+    return 'received'
+
+## RUN CONFIG ##
+
+if __name__ == "__main__": # execute app as principal class
+    app.run(debug = True) # run mode
