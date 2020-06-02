01eb9d8f8a173ce2758f94340fdca03d7a6f5830
Oswaldo Peralta <oswaldoperalta@gmail.com>
2020-06-02 05:59:02 -0400

    config to comunicate with db

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
-
 from flask import Flask # framework to build webapps
 from flask-sqlalchemy import SQLAlchemy # ORM for database layer abstraction
 from flask_marshmallow import Marshmallow # allow db schema creation
+
+app = flask(__name__) #set aplication name
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://levely:Elex.184@35.215.208.159/flaskmysql' # pass db parameters
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #skip warning messages on runtime
+
+db = SQLALCHEMY(app) # pass config to database variable
+ma = Marshmallow(app) # pass config to schema variable
