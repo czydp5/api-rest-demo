93119ee070fc9c8f691af0f217bf7a0be32b9920
Oswaldo Peralta <oswaldoperalta@gmail.com>
2020-06-02 05:52:49 -0400

    importing frameworks and objects

---
+++ src/app.py
@@ -0,0 +1,4 @@
+
+from flask import Flask # framework to build webapps
+from flask-sqlalchemy import SQLAlchemy # ORM for database layer abstraction
+from flask_marshmallow import Marshmallow # allow db schema creation
